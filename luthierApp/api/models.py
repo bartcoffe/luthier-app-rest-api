d8a38c48ef50e6d3bb80d4319d5878d41712e7f2
bart <bartkawa@proton.me>
2023-05-07 11:26:52 +0200

    set up project structure

---
+++ luthierApp/api/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+class UserRepairListing(models.Model):
+
+    def __str__(self):
+        return 'xd'
+
+class ListingPictureUrl(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class Category(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class Brands(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class Status(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class PaymentMethod(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class User(models.Model): # extend from django user!!!!!!!!!!!!!!!
+
+    def __str__(self):
+        return 'xd'
+    
+class ShippingAddress(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class LuthierOrder(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
+class OrderStatusHistory(models.Model):
+
+    def __str__(self):
+        return 'xd'
+    
